dca526f296f2c25324774ef32bbc6dbd9047f578
D-speedster <mrspeed717@gmail.com>
2024-04-24 14:49:01 +0330

    create news page

---
+++ app/News/page.js
@@ -0,0 +1,229 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import PageHeading from '@/components/info/PageHeading'
+import React from 'react'
+
+// import '../News.module.css'
+export default function page() {
+    return (
+        <>
+            <Header></Header>
+            <PageHeading></PageHeading>
+            <div className='container mt-4'>
+                <div className='row gx-2 header-news'>
+                    <div className='col-lg-6'>
+                        <div className='news-header-item'>
+                            <img src='/images/top-game-01.jpg' className='img-fluid rounded-3' />
+                        </div>
+                    </div>
+                    <div className='col-lg-3'>
+                        <div className='news-header-item'>
+                            <img src='/images/banner-image.jpg' className='img-fluid' />
+                        </div>
+                    </div>
+                    <div className='col-lg-3'>
+                        <div className='news-header-item'>
+                            <img src='/images/single-game.jpg' className='img-fluid' />
+                        </div>
+                    </div>
+                </div>
+                <br />
+                <div className='row gx-0 section-news'>
+                    <div className='col-lg-3 '>
+                        <div className=' '>
+                            <div className='container pt-3'>
+                                <div className='item-news'>
+                                    <div className='card text-center' style={{ border: 'none' }}>
+                                        <img src='/images/news/1.webp' className='card-img' />
+                                        <span className='card-title'>راهنمای خرید کامپیوتر گیمینگ رده حرفه ای</span>
+                                    </div>
+
+                                </div>
+
+                                <div className='item-news mt-2'>
+                                    <div className='card text-center' style={{ border: 'none' }}>
+                                        <img src='/images/news/avatar-frontiers-of-pandora.webp' className='card-img' />
+                                        <span className='card-title'>راهنمای خرید کامپیوتر گیمینگ رده حرفه ای</span>
+                                    </div>
+
+                                </div>
+                                <div className='item-news mt-2'>
+                                    <div className='card text-center' style={{ border: 'none' }}>
+                                        <img src='/images/news/dune-2-paul-leader.webp' className='card-img' />
+                                        <span className='card-title'>راهنمای خرید کامپیوتر گیمینگ رده حرفه ای</span>
+                                    </div>
+
+                                </div>
+                                <div className='item-news mt-2'>
+                                    <div className='card text-center' style={{ border: 'none' }}>
+                                        <img src='/images/news/001.webp' className='card-img' />
+                                        <span className='card-title'>راهنمای خرید کامپیوتر گیمینگ رده حرفه ای</span>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className='col-lg-6'>
+                        <div className='' >
+                            <div className='container'>
+                                <div className='main-news-item pt-3'>
+                                    <div className='card' style={{ border: 'none' }}>
+                                        <div className='row gx-0'>
+                                            <div className='col-lg-4'>
+                                                <img src='/images/news/house.webp' className='rounded-start' height='100%' />
+                                            </div>
+                                            <div className='col-lg-8 ps-2 pt-2'>
+                                                <h3 className='card-title' style={{ color: '#000 !important', fontSize: '16px', fontWeight: '600' }}>
+                                                    ویدیو فصل دوم خاندان اژدها؛ دفاع از وارث برحق تاج‌وتخت
+                                                </h3>
+                                                <small>1403/02/05 Admin</small>
+                                                <p>
+                                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و    ...
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='main-news-item pt-3'>
+                                    <div className='card' style={{ border: 'none' }}>
+                                        <div className='row gx-0'>
+                                            <div className='col-lg-4'>
+                                                <img src='/images/news/aiden-pearce-in-watch-dogs-complete-edition.webp' className='rounded-start' height='100%' />
+                                            </div>
+                                            <div className='col-lg-8 ps-2 pt-2'>
+                                                <h3 className='card-title' style={{ color: '#000 !important', fontSize: '16px', fontWeight: '600' }}>
+                                                بازی‌های Watch Dogs به خاطره‌ها می‌پیوندند؟
+                                                </h3>
+                                                <small>1403/02/05 Admin</small>
+                                                <p>
+                                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و    ...
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='main-news-item pt-3'>
+                                    <div className='card' style={{ border: 'none' }}>
+                                        <div className='row gx-0'>
+                                            <div className='col-lg-4'>
+                                                <img src='/images/news/billie-eilish-fortnite.webp' className='rounded-start' height='100%' />
+                                            </div>
+                                            <div className='col-lg-8 ps-2 pt-2'>
+                                                <h3 className='card-title' style={{ color: '#000 !important', fontSize: '16px', fontWeight: '600' }}>
+                                                بازی‌های Watch Dogs به خاطره‌ها می‌پیوندند؟
+                                                </h3>
+                                                <small>1403/02/05 Admin</small>
+                                                <p>
+                                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و    ...
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='main-news-item pt-3'>
+                                    <div className='card' style={{ border: 'none' }}>
+                                        <div className='row gx-0'>
+                                            <div className='col-lg-4'>
+                                                <img src='/images/news/star-wars-jedi-survivor-ea-play.webp' className='rounded-start' height='100%' />
+                                            </div>
+                                            <div className='col-lg-8 ps-2 pt-2'>
+                                                <h3 className='card-title' style={{ color: '#000 !important', fontSize: '16px', fontWeight: '600' }}>
+                                                بازی‌های Watch Dogs به خاطره‌ها می‌پیوندند؟
+                                                </h3>
+                                                <small>1403/02/05 Admin</small>
+                                                <p>
+                                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و    ...
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='main-news-item pt-3'>
+                                    <div className='card' style={{ border: 'none' }}>
+                                        <div className='row gx-0'>
+                                            <div className='col-lg-4'>
+                                                <img src='/images/news/sonic-frontiers.webp' className='rounded-start' height='100%' />
+                                            </div>
+                                            <div className='col-lg-8 ps-2 pt-2'>
+                                                <h3 className='card-title' style={{ color: '#000 !important', fontSize: '16px', fontWeight: '600' }}>
+                                                بازی‌های Watch Dogs به خاطره‌ها می‌پیوندند؟
+                                                </h3>
+                                                <small>1403/02/05 Admin</small>
+                                                <p>
+                                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و    ...
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='main-news-item pt-3'>
+                                    <div className='card' style={{ border: 'none' }}>
+                                        <div className='row gx-0'>
+                                            <div className='col-lg-4'>
+                                                <img src='/images/news/house.webp' className='rounded-start' height='100%'/>
+                                            </div>
+                                            <div className='col-lg-8 ps-2 pt-2'>
+                                                <h3 className='card-title' style={{ color: '#000 !important', fontSize: '16px', fontWeight: '600' }}>
+                                                بازی‌های Watch Dogs به خاطره‌ها می‌پیوندند؟
+                                                </h3>
+                                                <small>1403/02/05 Admin</small>
+                                                <p>
+                                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و    ...
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='bg-white mt-2 rounded-3'>
+                                    1 2 3 4 5
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className='col-lg-3 '>
+                        <div className='  '>
+                            <div className='container pt-3 '>
+                                <div className='item-news'>
+                                    <div className='card text-center' style={{ border: 'none' }}>
+                                        <img src='/images/news/1.webp' className='card-img' />
+                                        <span className='card-title'>راهنمای خرید کامپیوتر گیمینگ رده حرفه ای</span>
+                                    </div>
+
+                                </div>
+
+                                <div className='item-news mt-2'>
+                                    <div className='card text-center' style={{ border: 'none' }}>
+                                        <img src='/images/news/avatar-frontiers-of-pandora.webp' className='card-img' />
+                                        <span className='card-title'>راهنمای خرید کامپیوتر گیمینگ رده حرفه ای</span>
+                                    </div>
+
+                                </div>
+                                <div className='item-news mt-2'>
+                                    <div className='card text-center' style={{ border: 'none' }}>
+                                        <img src='/images/news/dune-2-paul-leader.webp' className='card-img' />
+                                        <span className='card-title'>راهنمای خرید کامپیوتر گیمینگ رده حرفه ای</span>
+                                    </div>
+
+                                </div>
+                                <div className='item-news mt-2'>
+                                    <div className='card text-center' style={{ border: 'none' }}>
+                                        <img src='/images/news/001.webp' className='card-img' />
+                                        <span className='card-title'>راهنمای خرید کامپیوتر گیمینگ رده حرفه ای</span>
+                                    </div>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer></Footer>
+
+        </>
+    )
+}
--- components/Header.js
+++ components/Header.js
@@ -56,19 +56,21 @@ export default function Header() {
             </nav>
             <nav class="navbar navbar-expand-lg " >
               <div class="container-fluid">
-                <a class="navbar-brand" href="#">
-                  <div className='LogoWeb d-flex' style={{ marginTop: '5px' }}>
-                    <img src='/images/Logo5.png' style={{ height: '55px', width: '55px', aspectRatio: '1.5' }} />
-                    <span className='mt-3' style={{ fontWeight: '600', color: '#FFF', fontSize: '14px' }}>اسپارتا</span>
-                  </div>
-                </a>
+                
+                  <Link className='navbar-brand' href='/'>
+                    <div className='LogoWeb d-flex' style={{ marginTop: '5px' }}>
+                      <img src='/images/Logo5.png' style={{ height: '55px', width: '55px', aspectRatio: '1.5' }} />
+                      <span className='mt-3' style={{ fontWeight: '600', color: '#FFF', fontSize: '14px' }}>اسپارتا</span>
+                    </div>
+                  </Link>
+               
                 <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarText">
                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                      <Link style={{color : '#FFF '}} href="/" className="nav-link active">
+                      <Link style={{ color: '#FFF ' }} href="/" className="nav-link active">
                         خانه
                       </Link>
 
@@ -77,16 +79,16 @@ export default function Header() {
                       <Link style={{ color: '#FFF ' }} className="nav-link" href="products">فروشگاه ما</Link>
                     </li>
                     <li class="nav-item">
-                      <Link style={{ color: '#FFF ' }} className="nav-link" href="products">اخبار</Link>
+                      <Link style={{ color: '#FFF ' }} className="nav-link" href="News">اخبار</Link>
                     </li>
                     <li class="nav-item">
                       <Link style={{ color: '#FFF ' }} className="nav-link" href="contact">تماس با ما</Link>
                     </li>
                   </ul>
-                  <span style={{color : '#FFF '}} class="navbar-text me-4">
+                  <span style={{ color: '#FFF ' }} class="navbar-text me-4">
                     ثبت نام / لاگین
-                  </span> 
-                  <span style={{color : '#FFF '}} class="navbar-text">
+                  </span>
+                  <span style={{ color: '#FFF ' }} class="navbar-text">
                     سبد خرید
                   </span>
                 </div>
